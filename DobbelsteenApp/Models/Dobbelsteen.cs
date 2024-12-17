53d162ca6b4d3aacc78cb9fa32d2b64de7245a8e
Ellie van Hugten <2405715hugten@zuyd.nl>
2024-12-17 11:54:45 +0100

    Dobbelsteen class blueprint

---
+++ DobbelsteenApp/Models/Dobbelsteen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobbelsteenApp.Models
+{
+    internal class Dobbelsteen
+    {
+        public int AantalKanten { get; set; }
+        public string Kleur {  get; set; }
+        public string Vorm {  get; set; }
+        public string Oogkleuren { get; set; }
+        public string Symbolen { get; set; }
+        public int AantalMogelijkheden { get; set; }
+    }
+}
--- DobbelsteenApp/Program.cs
+++ DobbelsteenApp/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace DobbelsteenApp
+﻿using DobbelsteenApp.Models;
+
+namespace DobbelsteenApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dobbelsteen dobbelsteen = new Dobbelsteen();
+            dobbelsteen.Kleur = "Geel";
+            dobbelsteen.AantalKanten = 4;
+            dobbelsteen.AantalMogelijkheden = 4;
+            dobbelsteen.Symbolen = "Getallen";
+            dobbelsteen.Oogkleuren = "Zwart";
+            dobbelsteen.Vorm = "Pyramide";
+
+            Console.WriteLine($"Kleur: {dobbelsteen.Kleur}\nKleur ogen: {dobbelsteen.Oogkleuren}\nAantal kanten: {dobbelsteen.AantalKanten}\nAantal mogelijkheden: {dobbelsteen.AantalMogelijkheden}\nSoort symbolen: {dobbelsteen.Symbolen}\nVorm: {dobbelsteen.Vorm}");
+            Console.ReadKey();
         }
     }
 }
